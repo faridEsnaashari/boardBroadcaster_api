6e54f64dfb1cb96c8b1fc7b7bee997241b209adf
faridEsnaashari <faride13578642@gmail.com>
2021-04-14 19:06:01 +0430

    express app. also add some data from config file to global and add v1 route

---
+++ app.js
@@ -0,0 +1,25 @@
+global.env = require('./config').env;
+
+global.paths = require('./config').paths;
+
+const express = require("express");
+const cors = require("cors")
+const bodyParser = require("body-parser");
+const responser = require(`${ global.paths.tools.responser }`);
+
+const v1 = require(`${ global.paths.v1.path }`);
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(responser());
+
+app.use("/v1", v1);
+app.use("/", v1);
+
+app.use((req, res) => {
+    res.responser(404, "wrong route or method");
+});
+
+module.exports = app;
