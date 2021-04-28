27dd2c7ea3b791c15aa5861c02876e34e5ddc143
faridEsnaashari <faride13578642@gmail.com>
2021-04-28 17:29:50 +0430

    a tool for generating a random verification code

---
+++ api/tools/randomVerifationCodeGenerator.js
@@ -0,0 +1,11 @@
+const generateRandomVerificationCode = digit =>  {
+    let digitA = 1;
+    let digitB = 9; 
+    for(let i = 1; i < digit; i++){
+        digitA *= 10;
+        digitB *= 10;
+    }
+    return Math.floor(Math.floor(digitA + Math.random() * digitB)) + 1;
+};
+
+module.exports = generateRandomVerificationCode;
