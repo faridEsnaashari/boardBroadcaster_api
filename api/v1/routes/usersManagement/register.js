4cf59d72961ed8482ca19e7242f25d7bead74e8a
faridEsnaashari <faride13578642@gmail.com>
2021-04-28 17:23:33 +0430

    add a validator for post method of the register route

--- api/v1/routes/usersManagement/register.js
+++ api/v1/routes/usersManagement/register.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const postController = require(`${ global.paths.v1.controllers.register }/post`);
 
-router.post("/", postController);
+const postValidator = require(`${ global.paths.v1.validators.register }/post`);
+
+router.post("/", postValidator, postController);
 
 module.exports = router;
