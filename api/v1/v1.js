16a7f2ef26a721fc16c0b3380af6d4c6964af213
faridEsnaashari <faride13578642@gmail.com>
2021-05-03 10:53:49 +0430

    add resendVerificationMail route

--- api/v1/v1.js
+++ api/v1/v1.js
@@ -2,7 +2,9 @@ const express = require ("express");
 const router = express.Router();
 
 const registerRoute = require(`${ global.paths.v1.routes.register }`);
+const resendVerificationMailRoute = require(`${ global.paths.v1.routes.resendVerificationMail }`);
 
 router.use("/register", registerRoute);
+router.use("/resendVerificationMail", resendVerificationMailRoute);
 
 module.exports = router;
