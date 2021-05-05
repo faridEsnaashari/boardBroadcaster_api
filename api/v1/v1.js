df470c96b709c40f7714a1eb62f62b44ad711a21
faridEsnaashari <faride13578642@gmail.com>
2021-05-05 12:37:33 +0430

    add verifyEmail route

--- api/v1/v1.js
+++ api/v1/v1.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const registerRoute = require(`${ global.paths.v1.routes.register }`);
 const resendVerificationMailRoute = require(`${ global.paths.v1.routes.resendVerificationMail }`);
+const verifyEmailRoute = require(`${ global.paths.v1.routes.verifyEmail }`);
 
 router.use("/register", registerRoute);
 router.use("/resendVerificationMail", resendVerificationMailRoute);
+router.use("/verifyEmail/", verifyEmailRoute);
 
 module.exports = router;
