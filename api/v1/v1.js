b5138e55e6084572f1f77584701ded71036da8f2
faridEsnaashari <faride13578642@gmail.com>
2021-05-06 15:54:39 +0430

    add login route

--- api/v1/v1.js
+++ api/v1/v1.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const registerRoute = require(`${ global.paths.v1.routes.register }`);
 const resendVerificationMailRoute = require(`${ global.paths.v1.routes.resendVerificationMail }`);
 const verifyEmailRoute = require(`${ global.paths.v1.routes.verifyEmail }`);
+const loginRoute = require(`${ global.paths.v1.routes.login }`);
 
 router.use("/register", registerRoute);
 router.use("/resendVerificationMail", resendVerificationMailRoute);
 router.use("/verifyEmail/", verifyEmailRoute);
+router.use("/login/", loginRoute);
 
 module.exports = router;
