c2df7d42502c8acbfd92f69def2ed88727e7290d
faridEsnaashari <faride13578642@gmail.com>
2021-05-06 13:45:42 +0430

    add get controller of this route

--- api/v1/routes/usersManagement/verifyEmail.js
+++ api/v1/routes/usersManagement/verifyEmail.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const getController = require(`${ global.paths.v1.controllers.verifyEmail }/get`);
 const tokenUnpacker = require(`${ global.paths.middlewares.tokenUnpacker }`);
 
-router.get("/:verificationToken", tokenUnpacker);
+router.get("/:verificationToken", tokenUnpacker, getController);
 
 module.exports = router;
