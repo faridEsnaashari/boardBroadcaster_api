8b20288a62259c3449714cd8ab369a1d560c1803
faridEsnaashari <faride13578642@gmail.com>
2021-05-25 21:15:53 +0430

    A post controller for login route

---
+++ api/v1/controllers/usersManagement/login/post.js
@@ -0,0 +1,35 @@
+const { createJWT } = require(`${ global.paths.tools.helper }`);
+const { UserModel } = require(`${ global.paths.v1.mongooseModels }`);
+const { INTERNAL_ERR, SUCCESS_MSG, UNAUTHORIZED_ERR } = require(`${ global.paths.tools.statusCodes }`);
+
+const postController = async(req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const user = await UserModel.findOne({ email });
+        if(!user){
+            return res.responser(UNAUTHORIZED_ERR, "email or password is incurrect");
+        }
+        
+        if(!user.verified){
+            return res.responser(UNAUTHORIZED_ERR, "email not verified yet");
+        }
+
+        if(!user.checkPassword(password)){
+            return res.responser(UNAUTHORIZED_ERR, "email or password is incurrect");
+        }
+
+        const payload = { name: user.name, email: user.email };
+        const token = await createJWT(payload);
+
+        return res.responser(SUCCESS_MSG, "user loged in successfully", { userToken: token });
+
+    }
+    catch(err){
+        console.error(err);
+
+        return res.responser(INTERNAL_ERR, "internal server error");
+    }
+};
+
+module.exports = postController;
