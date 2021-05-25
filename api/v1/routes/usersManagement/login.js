e45830aed0bb84d53dc61298ae664589afcb1192
faridEsnaashari <faride13578642@gmail.com>
2021-05-25 21:15:28 +0430

    add post controller

--- api/v1/routes/usersManagement/login.js
+++ api/v1/routes/usersManagement/login.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const postController = require(`${ global.paths.v1.controllers.login }/post`);
 const postValidator = require(`${ global.paths.v1.validators.login }/post`);
 
-router.post('/', postValidator);
+router.post('/', postValidator, postController);
 
 module.exports = router;
