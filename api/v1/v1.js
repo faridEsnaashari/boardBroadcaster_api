3c9d54cf1bbeecbc2c9244e175ce90cd50c12e0d
faridEsnaashari <faride13578642@gmail.com>
2021-09-09 22:42:30 +0430

    User route was added

--- api/v1/v1.js
+++ api/v1/v1.js
@@ -5,10 +5,12 @@ const registerRoute = require(`${ global.paths.v1.routes.register }`);
 const resendVerificationMailRoute = require(`${ global.paths.v1.routes.resendVerificationMail }`);
 const verifyEmailRoute = require(`${ global.paths.v1.routes.verifyEmail }`);
 const loginRoute = require(`${ global.paths.v1.routes.login }`);
+const userRoute = require(`${ global.paths.v1.routes.user }`);
 
 router.use("/register", registerRoute);
 router.use("/resendVerificationMail", resendVerificationMailRoute);
 router.use("/verifyEmail/", verifyEmailRoute);
 router.use("/login/", loginRoute);
+router.use("/user", userRoute);
 
 module.exports = router;
