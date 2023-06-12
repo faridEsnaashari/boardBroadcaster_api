e93598290473593b6cf8b78c07f9ca25b92fa23b
faridEsnaashari <faride13578642@gmail.com>
2023-06-12 15:24:45 +0330

    route controller and validator for get board by identifier

---
+++ api/v1/validators/board/getByIdentifier.js
@@ -0,0 +1,15 @@
+const { IsUndefinedOrNull } = require(`${ global.paths.tools.validationTools }`);
+const { VALIDATION_ERR } = require(`${ global.paths.tools.statusCodes }`);
+
+const getValidator = (req, res, next) => {
+    const { id } = req.params;
+    console.log(id);
+
+    if(IsUndefinedOrNull(id)){
+        return res.responser(VALIDATION_ERR, "undefined field", null, { field: "id" });
+    }
+
+    next();
+};
+
+module.exports = getValidator;
