1ecf26d6be04aca34815560600c7e62ea17c8f95
faridEsnaashari <faride13578642@gmail.com>
2023-06-16 14:58:45 +0330

    socket configs have been added. new socket server is established

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const http = require("http");
 const app = require("./app");
+const Socket = require("./api/socket/socket");
 
 const server = http.createServer(app);
+
+const socketIo = new Socket(server, {
+    cors: {
+        origin: global.env.SOCKET.CORS.ORIGIN,
+    }
+});
 
 server.listen(global.env.GENERAL.SERVER_PORT, () => console.log(`listen on port ${ global.env.GENERAL.SERVER_PORT }`));
