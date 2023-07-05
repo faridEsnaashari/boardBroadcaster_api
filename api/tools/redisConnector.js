e012158c09b3216ca0a34e03fd3dcf11b369db4b
faridEsnaashari <faride13578642@gmail.com>
2023-07-05 17:28:44 +0330

    redis connector

---
+++ api/tools/redisConnector.js
@@ -0,0 +1,19 @@
+const { createClient } = require("redis");
+
+const client = createClient({ url: global.env.REDIS.CONNECTION_CONFIG });
+
+client.on("error", err => console.error(err));
+
+const connect = () => {
+    client.connect()
+        .then(() => {
+            console.log("###############################");
+            console.log("Redis connected successfully");
+            console.log("###############################");
+        });
+}
+
+module.exports = {
+    client,
+    connect,
+};
