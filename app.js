d5dc90e18e5306485877eea2f68b784f165e1c23
faridEsnaashari <faride13578642@gmail.com>
2023-09-21 10:52:02 +0330

    cookie-parser added. cors updated. v2 routes added

--- app.js
+++ app.js
@@ -6,17 +6,26 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const responser = require(`${global.paths.middlewares.responser}`);
+const cookieParser = require("cookie-parser");
 require(`${global.paths.tools.mongooseConnector}`);
 require(`${global.paths.tools.redisConnector}`).connect();
 
 const v1 = require(`${global.paths.v1.path}`);
+const v2 = require(`${global.paths.v2.path}`);
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: [global.env.GENERAL.APP_URL],
+  })
+);
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(responser());
 
+app.use("/v2", v2);
 app.use("/v1", v1);
 app.use("/", v1);
 
